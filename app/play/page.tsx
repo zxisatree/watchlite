1d5eff0a070423152c2eae3c1654745366cfb9f0
zxisatree <62459441+zxisatree@users.noreply.github.com>
2024-12-28 14:39:04 +0800

    Add play page

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -1,10 +1,18 @@
 "use client";
+import { useSearchParams } from "next/navigation";
 import YouTube from "react-youtube";
 
 export default function Play() {
-  return (
-    <div className="w-full h-full flex flex-col justify-center items-center">
-      <YouTube videoId="eeZzCoghZzw" />
-    </div>
-  );
+    const searchParams = useSearchParams();
+    const videoId = searchParams.get("v") || "eeZzCoghZzw";
+
+    function scaleWidthAndHeight(factor: number) {
+        return { height: 390 * factor, width: 640 * factor };
+    }
+
+    return (
+        <div className="w-full h-full flex flex-col justify-center items-center">
+            <YouTube videoId={videoId} opts={scaleWidthAndHeight(2)} />
+        </div>
+    );
 }
--- app/searchResult.tsx
+++ app/searchResult.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 type SearchResultProps = {
   result: gapi.client.youtube.SearchResult;
 };
 
 export default function SearchResult({ result }: SearchResultProps) {
-  return <div>{result.snippet?.title || "Title not found"}</div>;
+  return (
+    <Link href={`/play?v=${result.id?.videoId}`}>
+      {result.snippet?.title || "Title not found"}
+    </Link>
+  );
 }
